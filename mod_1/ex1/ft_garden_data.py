9cec0ee13c16f04d1f81e9a44a5dea9e9eca079b
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-02-18 17:54:58 -0300

    Explicitly specifying return types and variable types. Adding a comment to the function. add list for ex1

--- mod_1/ex1/ft_garden_data.py
+++ mod_1/ex1/ft_garden_data.py
@@ -1,22 +1,27 @@
 class Plant:
-    def __init__(self, name, height, age):
+    """represents a plant with a name, height, and age."""
+
+    def __init__(self, name: str, height: int, age: int):
+        "initializes the plants attributes"
         self.name = name
         self.height = height
         self.age = age
 
     def display_info(self):
+        """Display formatted plant information."""
         print(f"{self.name}: {self.height}cm, {self.age}days old")
 
 def main():
+    """Creates and displays gardens log."""
     print("=== Garden Plant Registry ===")
+    plants = [
+        Plant("Rose", 25, 30),
+        Plant("Sunflower", 80, 45),
+        Plant("Cactus", 15, 120)
+    ]
 
-    rose = Plant("Rose", 25, 30)
-    sunflower = Plant("Sunflower", 80, 45)
-    cactus = Plant("Cactus", 15, 120)
-
-    rose.display_info()
-    sunflower.display_info()
-    cactus.display_info()
+    for plant in plants:
+        plant.display()
 
 if __name__ == "__main__":
     main()
