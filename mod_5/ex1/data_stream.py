2563da36e5138f7fab4ad948ddaffc231585b9c4
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-03-28 16:37:16 -0300

    for mod_5 ex1: DataStream finished

--- mod_5/ex1/data_stream.py
+++ mod_5/ex1/data_stream.py
@@ -11,3 +11,24 @@ class DataStream(ABC):
     def __init__(self, stream_id: str) -> None:
         """Initialize stream with identifier."""
         self.stream_id: str = stream_id
+
+    def process_batch(self, data_batch: List[Any]) -> str:
+        """Process a batch of data."""
+        pass
+
+    def filter_data(
+            self, data_batch: List[Any], criteria: Optional[str] = None
+            ) -> List[Any]:
+        """Filter data based on criteria."""
+        if criteria is None:
+            return data_batch
+
+        return [item for item in data_batch if criteria in str(item)]
+
+    def get_stats(self) -> Dict[str, Union[str, int, float]]:
+        """Return stream statistics."""
+
+        return {
+            "stream_id": self.stream_id,
+            "status": "active"
+        }
