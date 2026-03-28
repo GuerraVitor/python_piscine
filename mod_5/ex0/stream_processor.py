15ecbc02fb8f80f8c7d0a762599c8c4442d8eee3
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-03-28 01:42:58 -0300

    add numeric validation

--- mod_5/ex0/stream_processor.py
+++ mod_5/ex0/stream_processor.py
@@ -3,25 +3,38 @@ from typing import Any
 
 
 class DataProcessor(ABC):
+    @abstractmethod
+    def process(self, data: Any) -> str:
+        """Process data and return result string."""
+        pass
 
     @abstractmethod
-    def process(self, data: Any) -> str
-        """Process data and return result string."""
-        DataProcessor
-    @abstractmethod 
-    def validate(self, data: Any) -> bool
-    """Validade if data is appropriate for this processor."""
+    def validate(self, data: Any) -> bool:
+        """Validade if data is appropriate for this processor."""
+        pass
 
-    def format_ouput(self, result: str) -> str
-    """Format the output string."""
-    return (f"Ouput: {result}")
+    def format_ouput(self, result: str) -> str:
+        """Format the output string."""
+        return f"Ouput: {result}"
+
 
 class NumericProcessor(DataProcessor):
     """Specialized processor for list with numeric values."""
 
-    def validate(self, data: Any) -> bool
-    """Validate if data is a list of numbers."""
-    
+    def validate(self, data: Any) -> bool:
+        """Validate if data is a list of numbers."""
+        try:
+            _ = data + []
+
+            count: int = 0
+            for i in data:
+                float(i)
+                count += 1
+
+            return count > 0
+
+        except (TypeError, ValueError):
+            return False
+
     def process(self, data: Any) -> str:
         """Calculate sum and average for a list of numbers."""
-
