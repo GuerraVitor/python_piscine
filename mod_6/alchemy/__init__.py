4b6c85ab74e067fc318d6451b0928e3922cde2bf
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-03-31 19:56:52 -0300

    mod6: created package file

---
+++ mod_6/alchemy/__init__.py
@@ -0,0 +1,6 @@
+"""Alchemy package for creating elemental objects."""
+
+__version__ = "1.0.0"
+__author__ = "Vitor Guerra"
+
+from .elements import create_fire, create_water
--- mod_6/alchemy/elements.py
+++ mod_6/alchemy/elements.py
@@ -5,13 +5,16 @@ def create_fire() -> str:
     """Create fire element."""
     return "Fire element created"
 
+
 def create_water() -> str:
     """Create water element."""
     return "Water element created"
 
+
 def create_earth() -> str:
     """Create earth element."""
     return "Earth element created"
+
 
 def create_air() -> str:
     """Create air element."""
