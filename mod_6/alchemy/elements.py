96ee9363b8fbffaccf58b2ff80090ee77785a3b9
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-04-04 21:50:52 -0300

    change mod6 to fit new subject version

--- mod_6/alchemy/elements.py
+++ mod_6/alchemy/elements.py
@@ -1,16 +1,6 @@
 """Module for creating elemental components."""
 
 
-def create_fire() -> str:
-    """Create fire element."""
-    return "Fire element created"
-
-
-def create_water() -> str:
-    """Create water element."""
-    return "Water element created"
-
-
 def create_earth() -> str:
     """Create earth element."""
     return "Earth element created"
