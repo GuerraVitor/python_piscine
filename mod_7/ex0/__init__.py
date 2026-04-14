9cd3ba11bec5550df7ee64f784ebb4088f6a73ab
GuerraVt <vitorhugoguerraguimaraes@gmail.com>
2026-04-14 05:51:17 -0300

    mod7: init file added

---
+++ mod_7/ex0/__init__.py
@@ -0,0 +1,5 @@
+"""Package exposing Pokémon factories."""
+
+from .factory import CreatureFactory, FireFactory, WaterFactory, GrassFactory
+
+__all__ = ["CreatureFactory", "FireFactory", "WaterFactory", "GrassFactory"]
